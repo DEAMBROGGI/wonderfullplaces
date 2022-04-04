f4f16ae71a0978ec30e9319226edb48d0fd9a171
DEAMBROGGI <adrian.m.deambroggi@gmail.com>
2022-04-04 15:39:33 -0300

    socket

--- config/validator.js
+++ config/validator.js
@@ -11,8 +11,8 @@ const validator = (req, res, next) => {
         email: joi.string().email({ minDomainSegments: 2 }).required().messages({
             'string.email': 'Formato incorrecto de email'
         }),
-        password: joi.string().max(30).min(6).pattern(/(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])/).required().messages({
-            "string.pattern.base": "La contraseña debe contener minimo una mayuscula, una minuscula y un numero",
+        password: joi.string().max(30).min(6).required().messages({
+            
             "string.min": "La contraseña debe contener minimo 6 caracteres alfanumericos",
             "string.max": "La contraseña no debe exceder de 30 caracteres alfanumericos"
         }),
